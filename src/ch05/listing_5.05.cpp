4701cf7110aaf045d8b68a42fd249e1383b276d8
derek liu <derekliu.pro@gmail.com>
2020-08-17 01:12:22 -0700

    add testcase to listing 5.05

--- src/ch05/listing_5.05.cpp
+++ src/ch05/listing_5.05.cpp
@@ -1,3 +1,4 @@
+#include <cstdlib>
 #include <atomic>
 #include <thread>
 #include <assert.h>
@@ -19,8 +20,7 @@ void read_y_then_x()
         ++z;
 }
 
-int main()
-{
+int testcase_memory_order_relaxed() {
     x = false;
     y = false;
     z = 0;
@@ -28,5 +28,74 @@ int main()
     std::thread b(read_y_then_x);
     a.join();
     b.join();
-    assert(z.load() != 0);
+    return z.load() == 0;
+}
+
+int test(int loop) {
+    int i=1;
+    int res=0;
+    for(;i<=loop;++i) {
+        res=testcase_memory_order_relaxed();
+        stdlog.println(sep(""), i, "/", loop, sep(" "), ":", res);
+        if(res) break;
+    }
+    stdlog.println(res?"[FAIL]":"[PASS]", "in", sep(), i, "/", loop, sep(" "), " loops");
+    return res;
+}
+
+std::atomic<int> a,b,c;
+std::atomic_flag run = ATOMIC_FLAG_INIT;
+
+void increase_a_then_b(int loop)
+{
+    while(loop-->0 && run.test_and_set(std::memory_order_relaxed)) {
+        a.fetch_add(1, std::memory_order_relaxed);
+        b.fetch_add(1, std::memory_order_relaxed);
+    }
+}
+
+void read_a_then_b(int loop)
+{
+    int x=0,y=0;
+    while(loop>0&&(x=a.load(std::memory_order_relaxed))>=(y=b.load(std::memory_order_relaxed))) --loop;
+    if(loop) {
+        run.clear(std::memory_order_relaxed);
+        c.fetch_add(1);
+        stdlog.println(sep(),"x=",x," / y=",y,sep(" "));
+    }
+}
+
+int testcase_memory_order_relaxed2() {
+    a=0;
+    b=0;
+    c=0;
+    int loop=10000;
+    run.test_and_set();
+    std::thread ta(increase_a_then_b, loop);
+    std::thread tb(read_a_then_b, loop);
+    ta.join();
+    tb.join();
+    return c.load();
+}
+
+int test2(int loop) {
+    int i=1;
+    int res=0;
+    for(;i<=loop;++i) {
+        res=testcase_memory_order_relaxed2();
+        stdlog.println(sep(""), i, "/", loop, sep(" "), ":", res);
+        if(res) break;
+    }
+    stdlog.println(res?"[FAIL]":"[PASS]", "in", sep(), i, "/", loop, sep(" "), " loops");
+    return res;
+}
+
+int main(int argc, char **argv)
+{
+    int loop=10;
+    if(argc>1) loop=strtol(argv[1],nullptr,0);
+    test(loop);
+    stdlog.println("###################################################");
+    test2(loop);
+    return 0;
 }
--- src/utils/log.h
+++ src/utils/log.h
@@ -3,16 +3,53 @@
 #include <iostream>
 #include <fstream>
 #include <mutex>
+#include <functional>
+
+class Log;
+class LogModifier {
+    std::function<void(Log&)> f_;
+public:
+    explicit LogModifier(std::function<void(Log&)> f):f_(f){}
+    inline void operator()(Log & log) const {f_(log);}
+};
 
 class Log {
     std::mutex mu_;
     std::ostream &os_;
+    std::string sep_;
 public:
-    explicit Log(std::ostream &os):os_(os){}
+    explicit Log(std::ostream &os, const std::string &sep=" "):os_(os),sep_(sep){}
+
+    inline void setSep(const std::string & sep) {sep_=sep;}
+    inline const std::string & getSep() const {return sep_;}
+    inline const std::string & getSep() {return sep_;}
+
+private:
+    template<typename T, typename ...Tn> inline void write(const T& o, const Tn&...args) {
+        os_<<o; 
+        if(sizeof...(args)) {
+            if(!sep_.empty()) os_<<sep_;
+            write(args...);
+        }
+    }
+
+    template<typename ...Tn> inline void write(const LogModifier &f, const Tn&...args) {
+        f(*this);
+        if(sizeof...(args)) write(args...);
+    }
+
+    inline void write() {}
+
+public:
     template<typename T, typename ...Tn> inline Log & print(const T& o, const Tn&...args) {
         std::lock_guard<std::mutex> lg(mu_);
-        os_<<o; 
-        ((os_<<" "<<args),...);
+        write(o, args...);
+        return *this;
+    }
+
+    template<typename ...Tn> inline Log & print(const LogModifier &f, const Tn&...args) {
+        std::lock_guard<std::mutex> lg(mu_);
+        write(f, args...);
         return *this;
     }
 
@@ -24,3 +61,5 @@ public:
 };
 
 extern Log stdlog;
+
+inline LogModifier sep(std::string &&s="") {return LogModifier([&](Log &log) {log.setSep(s);});}
