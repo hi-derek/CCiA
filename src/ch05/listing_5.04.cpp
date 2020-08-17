2362b4945d583249a99f1762fd8aaf8b91751248
derek liu <derekliu.pro@gmail.com>
2020-08-16 20:14:16 -0700

    update cmakelist

--- src/ch05/listing_5.04.cpp
+++ src/ch05/listing_5.04.cpp
@@ -1,6 +1,9 @@
 #include <atomic>
 #include <thread>
 #include <assert.h>
+#include <iostream>
+
+using namespace std;
 
 std::atomic<bool> x,y;
 std::atomic<int> z;
@@ -19,30 +22,47 @@ void read_x_then_y()
 {
     while (!x.load(std::memory_order_seq_cst));
 
-    if(y.load(std::memory_order_seq_cst))
+    if(y.load(std::memory_order_seq_cst)) {
+        stdlog<<"read_x_then_y" "\n";
         ++z;
+    }
 }
 
 void read_y_then_x()
 {
     while (!y.load(std::memory_order_seq_cst));
 
-    if(x.load(std::memory_order_seq_cst))
+    if(x.load(std::memory_order_seq_cst)){
+        stdlog<<"read_y_then_x" "\n";
         ++z;
+    }
 }
+
+
+bool test(int loop) {
+    bool fail=false;
+    do {
+        x = false;
+        y = false;
+        z = 0;
+        std::thread a(write_x);
+        std::thread b(write_y);
+        std::thread c(read_x_then_y);
+        std::thread d(read_y_then_x);
+        a.join();
+        b.join();
+        c.join();
+        d.join();
+        fail=(z.load() == 0);
+        assert(!fail);
+    } while(--loop>0);
+    return fail?-1:0;
+}
+
 
 int main()
 {
-    x = false;
-    y = false;
-    z = 0;
-    std::thread a(write_x);
-    std::thread b(write_y);
-    std::thread c(read_x_then_y);
-    std::thread d(read_y_then_x);
-    a.join();
-    b.join();
-    c.join();
-    d.join();
-    assert(z.load() != 0);
+    test(10);
+
+    return 0;
 }
